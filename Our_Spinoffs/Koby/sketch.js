8c25a6763c6967d9a93ce9958128f6b17e6fe5dd
Koby <36042368+kwood3@users.noreply.github.com>
2019-05-25 22:19:51 -0400

    Add files via upload

---
+++ Our_Spinoffs/Koby/sketch.js
@@ -0,0 +1,157 @@
+// Tom Sealey
+// Original work by Daniel Shiffman
+// http://codingtra.in
+// Steering Text Paths
+// Video: https://youtu.be/4hA7G3gup-4
+
+var font;
+var density = 0.25;
+var densitySlider;
+var vehicles = [];
+var txt1 = "hi";
+var txt2 = "there";
+var textBool = true;
+var mouseClicks = 0;
+
+function preload() {
+  font = loadFont('AvenirNextLTPro-Demi.otf');
+}
+
+function setup() {
+  createCanvas(900, 300);
+  background(51);
+  colorMode(HSB);
+  densitySlider = createSlider(0.1, 1, 0.25, 0.01);
+  densitySlider.position(400,300);
+
+  var points = font.textToPoints(txt1, 100, 200, 192, {
+    sampleFactor: density
+  });
+
+  for (var i = 0; i < points.length; i++) {
+    var pt = points[i];
+    var vehicle = new Vehicle(pt.x, pt.y);
+    vehicles.push(vehicle);
+    }
+
+    createP("Press the spacebar...");
+
+    createP("Now click the mouse inside the canvas...");
+
+}
+
+function draw() {
+  background(51);
+  var density = densitySlider.value();
+    for (var i = 0; i < vehicles.length; i++) {
+      col = color(map(i, 0, vehicles.length, 0, 360), 100, 100);
+    var v = vehicles[i];
+    v.behaviors();
+    v.update();
+    v.setColor(col);
+    v.show();
+  }
+}
+
+function word1Update() {
+  txt1 = word1.value();
+}
+
+function mousePressed(){
+  if (mouseX < 900 && mouseY < 300){
+    if (mouseClicks <= 2) {
+      for (var i = 0; i < vehicles.length; i++){
+        var v = vehicles[i];
+        v.newTarget(random(width), random(height));
+      }
+    } else {
+      for (var i = 0; i < vehicles.length; i++){
+        var v = vehicles[i];
+        v.newTarget(v.pos.x, height);
+        mouseClicks = 0;
+      }
+    }
+    mouseClicks++;
+  }
+}
+
+function keyPressed() {
+  if (keyCode === 32) {
+      if (!textBool) {
+        var points = font.textToPoints(txt1, 100, 200, 192, {
+          sampleFactor: density
+        });
+        textBool = true;
+        console.log(points.length);
+
+        if (points.length > vehicles.length) {
+          for (var i =0; i < vehicles.length; i++) {
+              var pt = points[i];
+              var v = vehicles[i];
+              v.newTarget(pt.x, pt.y);
+            }
+            for (var i = vehicles.length; i < points.length; i++) {
+              var pt = points[i];
+              var vehicle = new Vehicle(pt.x, pt.y);
+              vehicles.push(vehicle);
+              }
+        } else if (points.length < vehicles.length){
+          for (var i = points.length; i < vehicles.length; i++) {
+            var v = vehicles[i];
+            v.newTarget(random(width), height);
+            }
+          // for (var i = vehicles.length - 1; i > points.length; i--) {
+          //   var v = vehicles[i];
+          //   v.newTarget(pt.x, height);
+          //   }
+            for (var i =0; i < points.length; i++) {
+            var pt = points[i];
+            var v = vehicles[i];
+            v.newTarget(pt.x, pt.y);
+          }
+        } else {
+            for (var i =0; i < points.length; i++) {
+            var pt = points[i];
+            var v = vehicles[i];
+            v.newTarget(pt.x, pt.y);
+          }
+       }
+     }
+
+     else {
+      var points = font.textToPoints(txt2, 100, 200, 192, {
+        sampleFactor: density
+      });
+      textBool = false;
+
+      if (points.length > vehicles.length) {
+        for (var i =0; i < vehicles.length; i++) {
+            var pt = points[i];
+            var v = vehicles[i];
+            v.newTarget(pt.x, pt.y);
+          }
+          for (var i = vehicles.length; i < points.length; i++) {
+            var pt = points[i];
+            var vehicle = new Vehicle(pt.x, pt.y);
+            vehicles.push(vehicle);
+            }
+      } else if (points.length < vehicles.length) {
+        for (var i = vehicles.length; i > points.length; i--) {
+          var v = vehicles[i];
+          v.newTarget(random(width), height);
+          }
+          for (var i =0; i < points.length; i++) {
+          var pt = points[i];
+          var v = vehicles[i];
+          v.newTarget(pt.x, pt.y);
+        }
+      } else {
+      for (var i =0; i < points.length; i++) {
+          var pt = points[i];
+          var v = vehicles[i];
+          v.newTarget(pt.x, pt.y);
+        }
+     }
+    }
+  }
+}
---
+++ Our_Spinoffs/Koby/vehicle.js
@@ -0,0 +1,83 @@
+// Tom Sealey
+// Original work by Daniel Shiffman
+// http://codingtra.in
+// Steering Text Paths
+// Video: https://youtu.be/4hA7G3gup-4
+
+function Vehicle(x, y) {
+    this.pos = createVector(random(width), height);
+  this.target = createVector(x, y);
+  this.vel = p5.Vector.random2D();
+  this.acc = createVector();
+  this.r = 8;
+  this.maxspeed = 10;
+  this.maxforce = 1;
+  this.col = color(360, 100, 100);
+}
+
+Vehicle.prototype.behaviors = function() {
+  var arrive = this.arrive(this.target);
+  var mouse = createVector(mouseX, mouseY);
+  var flee = this.flee(mouse);
+
+  arrive.mult(1);
+  flee.mult(5);
+
+  this.applyForce(arrive);
+  this.applyForce(flee);
+
+}
+
+Vehicle.prototype.applyForce = function(f) {
+  this.acc.add(f);
+}
+
+Vehicle.prototype.update = function() {
+    this.pos.add(this.vel);
+    this.vel.add(this.acc);
+    this.acc.mult(0);
+}
+
+Vehicle.prototype.newTarget = function(x, y) {
+  this.target = createVector(x, y);
+}
+
+Vehicle.prototype.setColor = function(c) {
+  if (c){
+    this.col = c;
+  }
+}
+
+Vehicle.prototype.show = function() {
+  stroke(this.col);
+  strokeWeight(this.r);
+  point(this.pos.x, this.pos.y);
+}
+
+
+Vehicle.prototype.arrive = function(target) {
+  var desired = p5.Vector.sub(target, this.pos);
+  var d = desired.mag();
+  var speed = this.maxspeed;
+  if (d < 100) {
+    speed = map(d, 0, 100, 0, this.maxspeed);
+  }
+  desired.setMag(speed);
+  var steer = p5.Vector.sub(desired, this.vel);
+  steer.limit(this.maxforce);
+  return steer;
+}
+
+Vehicle.prototype.flee = function(target) {
+  var desired = p5.Vector.sub(target, this.pos);
+  var d = desired.mag();
+  if (d < 50) {
+    desired.setMag(this.maxspeed);
+    desired.mult(-1);
+    var steer = p5.Vector.sub(desired, this.vel);
+    steer.limit(this.maxforce);
+    return steer;
+  } else {
+    return createVector(0, 0);
+  }
+}
